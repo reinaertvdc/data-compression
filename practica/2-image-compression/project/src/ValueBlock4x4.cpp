24448f8e36e84e3750fb1abcb2f31749affd5e21
cwout <cwout.coenen@gmail.com>
2017-11-03 16:32:03 +0100

    ValueBlock class updated to use int16_t instead of short

--- practica/2-image-compression/project/src/ValueBlock4x4.cpp
+++ practica/2-image-compression/project/src/ValueBlock4x4.cpp
@@ -11,8 +11,8 @@ const double ValueBlock4x4::dctTransformMatrix[4][4] = {{0.5,   0.5,    0.5,    
 const int ValueBlock4x4::zzPatternRow[16] = {0,0,1,2,1,0,0,1,2,3,3,2,1,2,3,3};
 const int ValueBlock4x4::zzPatternCol[16] = {0,1,0,0,1,2,3,2,1,0,1,2,3,3,2,3};
 
-ValueBlock4x4::ValueBlock4x4(short matrix[4][4]) : empty(false) {
-    std::memcpy(this->matrix, matrix, sizeof(short) * 16);
+ValueBlock4x4::ValueBlock4x4(int16_t matrix[4][4]) : empty(false) {
+    std::memcpy(this->matrix, matrix, sizeof(int16_t) * 16);
 }
 
 ValueBlock4x4::ValueBlock4x4() : empty(true) {}
@@ -22,22 +22,22 @@ void ValueBlock4x4::applyDct() {
         return;
     }
     double tmp[4][4];
-    for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < 4; j++) {
+    for (int r = 0; r < 4; r++) {
+        for (int c = 0; c < 4; c++) {
             double sum = 0.0;
             for (int k = 0; k < 4; k++) {
-                sum += ValueBlock4x4::dctTransformMatrix[i][k] * static_cast<double>(this->matrix[k][j]);
+                sum += ValueBlock4x4::dctTransformMatrix[r][k] * static_cast<double>(this->matrix[k][c]);
             }
-            tmp[i][j] = sum;
+            tmp[r][c] = sum;
         }
     }
-    for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < 4; j++) {
+    for (int r = 0; r < 4; r++) {
+        for (int c = 0; c < 4; c++) {
             double sum = 0.0;
             for (int k = 0; k < 4; k++) {
-                sum += tmp[i][k] * ValueBlock4x4::dctTransformMatrix[j][k];
+                sum += tmp[r][k] * ValueBlock4x4::dctTransformMatrix[c][k];
             }
-            this->matrix[i][j] = static_cast<short>(round(sum));
+            this->matrix[r][c] = static_cast<int16_t>(round(sum));
         }
     }
 }
@@ -46,48 +46,45 @@ void ValueBlock4x4::applyInverseDct() {
     if (this->isEmpty()) {
         return;
     }
-    double tmp[4][4] = {{0.0, 0.0, 0.0, 0.0},
-                        {0.0, 0.0, 0.0, 0.0},
-                        {0.0, 0.0, 0.0, 0.0},
-                        {0.0, 0.0, 0.0, 0.0}};
-    for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < 4; j++) {
+    double tmp[4][4];
+    for (int r = 0; r < 4; r++) {
+        for (int c = 0; c < 4; c++) {
             double sum = 0.0;
             for (int k = 0; k < 4; k++) {
-                sum += dctTransformMatrix[k][i] * static_cast<double>(this->matrix[k][j]);
+                sum += dctTransformMatrix[k][r] * static_cast<double>(this->matrix[k][c]);
             }
-            tmp[i][j] = sum;
+            tmp[r][c] = sum;
         }
     }
-    for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < 4; j++) {
+    for (int r = 0; r < 4; r++) {
+        for (int c = 0; c < 4; c++) {
             double sum = 0.0;
             for (int k = 0; k < 4; k++) {
-                sum += tmp[i][k] * dctTransformMatrix[k][j];
+                sum += tmp[r][k] * dctTransformMatrix[k][c];
             }
-            this->matrix[i][j] = static_cast<short>(round(sum));
+            this->matrix[r][c] = static_cast<int16_t>(round(sum));
         }
     }
 }
 
 void ValueBlock4x4::quantize(const ValueBlock4x4 &quant) {
-    for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < 4; j++) {
-            this->matrix[i][j] = static_cast<short>(nearbyint(
-                    static_cast<double>(this->matrix[i][j]) / static_cast<double>(quant.matrix[i][j])));
+    for (int r = 0; r < 4; r++) {
+        for (int c = 0; c < 4; c++) {
+            this->matrix[r][c] = static_cast<int16_t>(nearbyint(
+                    static_cast<double>(this->matrix[r][c]) / static_cast<double>(quant.matrix[r][c])));
         }
     }
 }
 
 void ValueBlock4x4::deQuantize(const ValueBlock4x4 &quant) {
-    for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < 4; j++) {
-            this->matrix[i][j] = static_cast<short>(static_cast<int>(this->matrix[i][j]) * static_cast<int>(quant.matrix[i][j]));
+    for (int r = 0; r < 4; r++) {
+        for (int c = 0; c < 4; c++) {
+            this->matrix[r][c] = static_cast<int16_t>(static_cast<int>(this->matrix[r][c]) * static_cast<int>(quant.matrix[r][c]));
         }
     }
 }
 
-bool ValueBlock4x4::zigzag(short* out) const {
+bool ValueBlock4x4::zigzag(int16_t* out) const {
     if (out == nullptr) return false;
     if (this->empty) return false;
     for (int i = 0; i < 16; i++) {
@@ -96,7 +93,7 @@ bool ValueBlock4x4::zigzag(short* out) const {
     return true;
 }
 
-ValueBlock4x4::ValueBlock4x4(const short *array) {
+ValueBlock4x4::ValueBlock4x4(const int16_t *array) {
     if (array == nullptr) return;
     for (int i = 0; i < 16; i++) {
         this->matrix[ValueBlock4x4::zzPatternRow[i]][ValueBlock4x4::zzPatternCol[i]] = array[i];
--- practica/2-image-compression/project/src/ValueBlock4x4.h
+++ practica/2-image-compression/project/src/ValueBlock4x4.h
@@ -8,25 +8,26 @@
 
 class ValueBlock4x4 {
 private:
-    short matrix[4][4];
+    int16_t matrix[4][4];
     bool empty;
     static const double dctTransformMatrix[4][4];
     static const int zzPatternRow[16];
     static const int zzPatternCol[16];
 public:
     ValueBlock4x4();
-    explicit ValueBlock4x4(short matrix[4][4]);
-    explicit ValueBlock4x4(const short *array);
+    explicit ValueBlock4x4(int16_t matrix[4][4]);
+    explicit ValueBlock4x4(const int16_t *array);
     virtual ~ValueBlock4x4() = default;
     void setFilled() { this->empty = false; }
     bool isEmpty() { return this->empty; }
-    short *getData() { return (short *)this->matrix; }
+    int16_t *getData() { return (int16_t *)this->matrix; }
     void applyDct();
     void applyInverseDct();
     void quantize(const ValueBlock4x4 &quant);
     void deQuantize(const ValueBlock4x4 &quant);
-    short getValue(int row, int col) const { if (this->empty) return 0; else return this->matrix[row][col]; }
-    bool zigzag(short* out) const;
+    int16_t getValue(int row, int col) const { if (this->empty) return static_cast<int16_t>(0); else return this->matrix[row][col]; }
+    short getValueAsShort(int row, int col) const { if (this->empty) return static_cast<short>(0); else return static_cast<short>(this->matrix[row][col]); }
+    bool zigzag(int16_t * out) const;
 };
 
 
