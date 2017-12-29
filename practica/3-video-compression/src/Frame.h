48692b725e6c1db7bc7ffc25e201a3d4517f0904
cwout <cwout.coenen@gmail.com>
2017-12-29 18:11:48 +0100

    comments

--- practica/3-video-compression/src/Frame.h
+++ practica/3-video-compression/src/Frame.h
@@ -134,20 +134,89 @@ private:
      * Valueblock array representing the pixels in the frame
      */
     ValueBlock4x4 **const blocks;
+
+    /**
+     * Load iframe blocks (ValueBlock4x4) from a buffer and decompress the blocks
+     * @param data the compressed frame data buffer
+     * @param compressedSize the size of the compressed frame data
+     * @param rle whether or not rle was used to compress the frame data
+     * @param quantMatrix the quantization matrix used to quantize the dct components
+     * @return true if success, false otherwise
+     */
     bool loadI(uint8_t *data, int compressedSize, bool rle, const ValueBlock4x4 &quantMatrix);
+
+    /**
+     * Load pframe macroblocks from a buffer and decompress the macroblocks
+     * @param vectorBuffer the buffer containing the (compressed) motion vectors
+     * @param vectorStreamSize the size of the motion vector buffer
+     * @param quantMatrix the quantization matrix used to quantize the dct components for the motion compensation blocks
+     * @param previousFrame the frame on which to base the motion vectors
+     * @param merange the merange parameter (see EncoderConfig::merange)
+     * @param motionCompensation whether or not motion compensation was used
+     * @return true if success, false otherwise
+     */
     bool loadP(uint8_t *vectorBuffer, int vectorStreamSize, const ValueBlock4x4 &quantMatrix,
                const Frame &previousFrame, uint16_t merange, bool motionCompensation);
+
+    /**
+     * Load pixels from ValueBlock4x4's into a continuous array
+     */
     void loadPixels() const;
+
+    /**
+     * Copy a 16x16 macroblock into a buffer
+     * @param buffer the buffer to copy to
+     * @param macroRow the macroblock row
+     * @param macroCol the macroblock column
+     */
     void readMacroBlock(int16_t *buffer, int macroRow, int macroCol) const;
 
+    /**
+     * Copy a buffer into a 16x16 macroblock
+     * @param buffer the buffer that contains the data
+     * @param macroRow the macroblock row
+     * @param macroCol the macroblock column
+     */
     void writeMacroBlock(int16_t *buffer, int macroRow, int macroCol);
 
+    /**
+     * Calculate the closest motion vector for a given macroblock
+     * @param macroBlock the macroblock data buffer to calculate motion vectors for
+     * @param row the row (in actual pixel size) which is the topmost row of the 16x16 macroBlock
+     * @param col the column (in actual pixel size) which is the leftmost column of the 16x16 macroBlock
+     * @param merange the merange parameter (see EncoderConfig::merange)
+     * @param matchRow out the topmost row (in actual pixel size) which contains the closest matching macroblock
+     * @param matchCol out the leftmost column (in actual pixel size) which contains the closest matching macroblock
+     */
     void findClosestMatch(int16_t *macroBlock, int row, int col, uint16_t merange, int &matchRow, int &matchCol) const;
 
+    /**
+     * Calculate the motion compensation data
+     * @param macroBlock in the macroblock to calculate the motion compensation for
+     *                   out the motion compensation values are stored in the buffer itself
+     * @param row the row (topmost, in actual pixel size) to compare the macroblock against
+     * @param col the column (lefmost, in actual pixel size) to compare the macroblock against
+     */
     void getMotionCompensation(int16_t *macroBlock, int row, int col);
 
+    /**
+     * Apply a motion compenstation to a specific macroblock in the frame
+     * @param macroBlock the motion compensation values to apply
+     * @param row the row (topmost, in actual pixel size) to apply the motion compensation to
+     * @param col the column (lefmost, in actual pixel size) to apply the motion compensation to
+     */
     void applyMotionCompensation(int16_t *macroBlock, int row, int col);
 
+    /**
+     * Find the difference between a macroblock buffer and a macroblock given by the left/top-most row/column
+     * If the difference is >= currentMinDifference, then return a value >= currentMinValue
+     * @param macroBlock the macroblock to compare against
+     * @param row the row (topmost, in actual pixel size) of the macroblock to compare
+     * @param col the column (leftmost, in actual pixel size) of the macroblock to compare
+     * @param currentMinDifference the difference value that should not be surpassed
+     * @return the difference between the macroblocks if the difference is smaller than currentMinDifference
+     *         otherwise a value >= currentMinDifference
+     */
     int getDifferenceIfLower(const int16_t *macroBlock, int row, int col, int currentMinDifference) const;
 };
 
